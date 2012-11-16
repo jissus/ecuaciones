d9ed22b762fff9f6e377c6a9466fb3678b6d85e8
jesus <jesus.tissera@gmail.com>
2012-11-16 00:36:07 -0300

    otra correcion de espacios

--- src/main/java/ar/edu/unlam/tallerweb/ecuaciones/IntegralDeArea.java
+++ src/main/java/ar/edu/unlam/tallerweb/ecuaciones/IntegralDeArea.java
@@ -20,13 +20,13 @@ public class IntegralDeArea {
 	public Double calcular(Ecuacion ecuacion, Double inicio, Double fin,
 			Double incremento) {
 		if(ecuacion!=null&&inicio!=null&&fin!=null&&incremento!=null){
-		Double resultado = 0.0;
-		Double x = 0.0;
-		Double mitadIncremento = (incremento.doubleValue()/2);
-		Double cantidadR  = (fin.doubleValue()-inicio.doubleValue())/incremento.doubleValue();
+		Double resultado=0.0;
+		Double x =0.0;
+		Double mitadIncremento=(incremento.doubleValue()/2);
+		Double cantidadR=(fin.doubleValue()-inicio.doubleValue())/incremento.doubleValue();
 		for(int i=0;i<cantidadR.doubleValue();i++){
-		x = inicio.doubleValue()+(i*incremento.doubleValue());
-		resultado = resultado.doubleValue() + (ecuacion.resolver(x+mitadIncremento));
+		x=inicio.doubleValue()+(i*incremento.doubleValue());
+		resultado=resultado.doubleValue()+(ecuacion.resolver(x+mitadIncremento));
 		}
 		return Math.abs((resultado.doubleValue()*incremento.doubleValue()));
 		}else{
